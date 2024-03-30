cf9e1b593a0bbf0c8da043c29389177ae45b3cbd
John Doe <quoctuan4545@gmail.com>
2024-03-30 23:39:00 +0700

    init project

--- DichNguocAPK.py
+++ DichNguocAPK.py
@@ -1,13 +1,9 @@
 # Dịch ngược file apk sử dụng androguard
 #
 from androguard.misc import AnalyzeAPK
-import os
-from pathlib import Path
 
-# Đường dẫn đến file APK
-apk_path = os.path.join(os.getcwd(), 'apk_test.apk')
-# phân tích APK
-a, d, dx = AnalyzeAPK(apk_path)
+a,d,dx = AnalyzeAPK('apk_test.apk')
+
 
 # hiển thị các activity
 print("Các activity trong APK:")
@@ -19,11 +15,11 @@ print("Các permission trong APK:")
 for i in a.get_permissions():
     print(i)
 
-# hiển thị các phương thức
-print("Các phương thức trong APK:")
+# hiển thị các lớp 
+for i in dx.get_classes():
+    print(i)
+
+# hiển thị các method
 for i in dx.get_methods():
     print(i)
- # hiển thị các dịch vụ
-print("Các dịch vụ trong APK:")
-for i in a.get_services():
-    print(i)
+
--- Modeldoc2vec.py
+++ Modeldoc2vec.py
@@ -17,6 +17,7 @@ cg = dx.get_call_graph()
 
 # tạo data để train model doc2vec
 class_method_strings = []
+
 # lấy ra tên lớp và các phương thức có trong đỉnh
 for node in cg.nodes():
     class_name = node.class_name
@@ -36,60 +37,9 @@ model.build_vocab(documents)
 model.train(documents, total_examples=model.corpus_count, epochs=model.epochs)
 
 # Chuyển đổi dữ liệu thành vector
-vectors = [model.infer_vector(word_tokenize(doc.lower())) for doc in df['class_and_methods']]
+vectors_of_node = [model.infer_vector(word_tokenize(doc.lower())) for doc in df['class_and_methods']]
 
 # In ra vector của một số văn bản
-for i, vec in enumerate(vectors):
+for i, vec in enumerate(vectors_of_node):
     print(f"Vector của văn bản {i+1}: {vec}")
    
-
-
-#in ra các đỉnh và các cạnh ra một file out_put.txt
-# with open("out_put.csv", "w") as f:
-#     for node in cg.nodes():
-#         f.write(node+"\n")
-
-# # vẽ đồ thị
-# nx.draw_networkx(cg, with_labels=False, node_size=10)
-# plt.show()
-# def getModuleName(name) :
-#     name = name[26:] 
-#     pos = name.find("->") 
-#     name = name[:pos - 1] 
-#     cnt = name.count("/") 
-#     nameList = name.split("/") 
-#     if cnt <= 2:
-#         name = nameList[0] 
-#     else :
-#         name = "/".join(nameList[:-2]) 
-#     return name.strip() 
-
-# def getPackageName(name) :
-#     name = name[26:] 
-#     pos = name.find("->") 
-#     name = name[:pos - 1] 
-#     cnt = name.count("/") 
-#     nameList = name.split("/") 
-#     if cnt <= 1 :
-#         name = nameList[0] 
-#     else :
-#         name = "/".join(nameList[:-1]) 
-#     return name.strip() 
-
-# def getClassName(name) :
-#     name = name[26:]
-#     pos = name.find("->") 
-#     return name[:pos - 1].strip()
-
-# def getmethodName(name) :
-#     name = name[26:] 
-#     pos = name.find("@") 
-#     name = name[:pos - 1] 
-#     return name.strip()
-
-# def getMethodName(name) :
-#     name = name[26:] 
-#     pos = name.find("->") 
-#     return name[pos + 2:].strip()
-
-
